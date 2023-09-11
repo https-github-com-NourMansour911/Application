c541af82fab1a8dd902be7ad061293477356ed20
Nour Mansour <seriousnoursf90@gmail.com>
2023-09-11 13:37:22 +0300

    create repo and failures files

--- lib/Features/Auth/data/repos/auth_repo.dart
+++ lib/Features/Auth/data/repos/auth_repo.dart
@@ -1,5 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:e_gem/core/errors/failures.dart';
+
 abstract class AuthRepo {
-  login();
-  register();
-  forget_password();
+  Future login();
+  Future<Either<Failure, String>> register();
+  Future forget_password();
 }
